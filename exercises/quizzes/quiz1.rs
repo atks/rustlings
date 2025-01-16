c52687b0b5f01aae2a1645b45afd95676dda9213
atks <atks@umich.edu>
2025-01-16 17:09:46 +0800

    updated

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,6 +11,13 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(no_apples: i32) -> i32 {
+    if no_apples <= 40 {
+        no_apples * 2
+    } else {
+        no_apples
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
